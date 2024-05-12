2f76d47c7edf94d43d914fcae8bb20d9a19ca40d
shainiRose <shainiu22@gmail.com>
2024-05-12 12:43:29 +0530

    code added

---
+++ app/src/main/java/com/example/thenotesapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.thenotesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import androidx.lifecycle.ViewModelProvider
+import com.example.thenotesapp.database.NoteDatabase
+import com.example.thenotesapp.repository.NoteRepository
+import com.example.thenotesapp.viewmodel.NoteViewModel
+import com.example.thenotesapp.viewmodel.ViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+
+    private fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = ViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+
+
+
+    }
+
+
+
+
+}
